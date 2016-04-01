97428683207be98b4e2f31d31665ff83450eb210
Moonandeye <atimis19@gmail.com>
2016-04-01 23:46:21 +0900

    分糖果卡關
    目前卡在一個點。
    分的次數是對的，但是結果是錯的
    大家應該都分到18顆，而不是3顆

--- share_candy.py
+++ share_candy.py
@@ -23,8 +23,63 @@ Created on Sun Mar 27 21:53:58 2016
 然後又是幾顆?
 
 """
+children = [10, 2, 8, 22, 16, 4, 10, 6, 14, 20]
 a = [i*2 for i in range(5)]
 b = [i*2-1 for i in range(1,6)]
 
 c = [a+b for a, b in zip(a,b)]
-print(a)
+#print(a)
+d = [int(i/2) for i in a]
+
+
+#print(d)
+
+def oddPlusOne(list):
+    new_list = []
+    for i in list:
+        if i % 2 == 0:
+            new_list.append(i)
+        else:
+            i = i + 1
+            new_list.append(i)
+    return(new_list)
+    
+test = oddPlusOne(children)
+
+e = sum(children)/len(children)
+f = len(children)
+
+def avgList(list):
+    avg = sum(list)/len(list)
+    return(avg)
+
+def notEnd(list):
+    avg = avgList(list)
+    index = 0
+    for i in list:
+        if i == avg:
+            pass
+        else:
+            index = index + 1
+    return(index)
+
+def candyNumGive(list):
+    give = oddPlusOne(list)
+    last = give.pop(len(give) - 1)
+    give.insert(0,last)
+    give = [int(i/2) for i in give]
+    return(give)
+    
+
+#temp = [children-give_to for children, give_to in zip(children, give_to)]
+times = 0
+while notEnd(children) != 0:
+    times = times + 1
+    children = oddPlusOne(children)
+    children = [int(i/2) for i in children]
+    receive = candyNumGive(children)
+    children = [children + receive for children, receive in zip(children, receive)]
+    
+print('times is ' + str(times))
+
+print("Result is " + str(children))
