ff36ff3ad1a992b6a79b49934cad208b508caa1d
Moonandeye <atimis19@gmail.com>
2016-04-10 23:55:56 +0900

    可能要想一個重復使用def，去初始化matrix

--- fishman_share_fish.py
+++ fishman_share_fish.py
@@ -13,3 +13,21 @@ Created on Fri Apr  8 22:53:32 2016
 在不倒出任何一條魚的情況，如何將魚和魚筐平分三份?
 
 """
+
+import numpy as np
+
+def initMatrix(dimension = 1, init = 0):
+    matrix = []    
+    for i in range(dimension):
+        for i in range(dimension):
+            pass
+    
+
+i = 0
+
+fall = i
+
+half_f = (3.5 - fall) * 2
+
+empty = 7 - fall - half_f
+
