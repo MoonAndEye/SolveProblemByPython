045a4000900d406cbd07773f9768f1dd50b43ae7
Moonandeye <atimis19@gmail.com>
2016-04-20 22:57:32 +0900

    可以印出解答

--- fishman_share_fish.py
+++ fishman_share_fish.py
@@ -9,31 +9,29 @@ import numpy as np
 a = np.array([1,1,1])
 b = np.array([1,0.5,0])
 
-print (a*b)
+#print (a*b)
 c = a*b
 d = np.dot(a,b)
-print(sum(c))
-print(d)
+#print(sum(c))
+#print(d)
 
 weight = [1,0.5,0]
 sol = []
 
 for i in range(1,4):
     for j in range(1,4):
+        temp = []
         fish_a = [i, 7-i-i, i]
         fish_b = [j, 7-j-j, j]
         fish_c = [7-i-j, 2*i+2*j-7,7-i-j]
-        if np.dot(weight, fish_a) == 3.5 and np.dot
+        if np.dot(weight, fish_a) == 3.5 and np.dot(weight, fish_b) == 3.5 and fish_c[0] >=0 and fish_c[1] >0:
+            temp.append(fish_a)
+            temp.append(fish_b)
+            temp.append(fish_c)
+            sol.append(temp)
+            
 
-(weight, fish_b) == 3.5 and fish_c[0] >=0 and fish_c
+for each in sol:
+    print(each)
 
-[1] >0:
-            sol.append(fish_a)
-            sol.append(fish_b)
-            sol.append(fish_c)
-            print(fish_a)
-            print(fish_b)
-            print(fish_c)
-            print("-------")
-#print(sol)
         
