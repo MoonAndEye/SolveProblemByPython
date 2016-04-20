64c7d93c9fa5c97ffe68ecf97e508ddaec1c1eba
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-04-20 15:12:01 +0900

    The repeat is not delete yet

--- fishman_share_fish.py
+++ fishman_share_fish.py
@@ -18,14 +18,22 @@ print(d)
 weight = [1,0.5,0]
 sol = []
 
-for i in range(1,8):
-    for j in range(1,8):
+for i in range(1,4):
+    for j in range(1,4):
         fish_a = [i, 7-i-i, i]
         fish_b = [j, 7-j-j, j]
         fish_c = [7-i-j, 2*i+2*j-7,7-i-j]
-        if np.dot(weight, fish_a) == 3.5 and np.dot(weight, fish_b) == 3.5 and fish_c[0] >=0 and fish_c[1] >0:
+        if np.dot(weight, fish_a) == 3.5 and np.dot
+
+(weight, fish_b) == 3.5 and fish_c[0] >=0 and fish_c
+
+[1] >0:
             sol.append(fish_a)
             sol.append(fish_b)
             sol.append(fish_c)
-print(sol)
+            print(fish_a)
+            print(fish_b)
+            print(fish_c)
+            print("-------")
+#print(sol)
         
